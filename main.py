603d280e9709fbd9dab78b3c2520c41cdcb64ff1
Islam0143 <sokoclash123@gmail.com>
2023-10-21 20:48:57 +0300

    updates

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from BFS import BreadthFirstSearch
 from DFS import DepthFirstSearch
 
-if __name__ == '__main__':
-    dfs = DepthFirstSearch()
-    print(dfs.execute("1253406785"))
+def readInput():
+    print("enter initial state")
+    strInput = ""
+    for i in range(3): strInput += input()
+    initialState = strInput.replace(" ", "")
+    initialState += str(initialState.find('0'))
+    return initialState
 
+if __name__ == '__main__':
+    initialState = readInput()
+    dfs = DepthFirstSearch()
+    print(dfs.execute(initialState))
     bfs = BreadthFirstSearch()
-    print(bfs.execute("1253406785"))
+    print(bfs.execute(initialState))
