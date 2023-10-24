82c204e827737faf094f718b27fc2d66f850be3d
Maro1123 <96551919+Maro1123@users.noreply.github.com>
2023-10-24 19:32:46 +0300

    Removed heuristic to its own file

--- Astar.py
+++ Astar.py
@@ -6,8 +6,8 @@ from State import State
 
 class AstarSearch(SearchAlgorithm):
 
-    def execute(self, initialState: State, initialFunc, heuristicFunc):
-        initialFunc(initialState)
+    def execute(self, initialState: State, heuristicFunc):
+        heuristicFunc(initialState)
         
         frontier = []
         self.nodesExpanded = set() 
@@ -31,37 +31,15 @@ class AstarSearch(SearchAlgorithm):
             
             for neighbour in self.findNeighbours(currState):
                 if neighbour.value not in frontierUexplored:
-                    self.initialManhattan(neighbour)
+                    heuristicFunc(neighbour)
                     heapq.heappush(frontier, neighbour)
+                    frontierUexplored.add(neighbour.value)
                     # frontierTable[neighbour.value] = neighbour
                 elif neighbour.value not in self.nodesExpanded:
                     # existingNeighbour = frontierTable[neighbour.value]
-                    self.initialManhattan(neighbour)
+                    heuristicFunc(neighbour)
                     heapq.heappush(frontier, neighbour)
+                    frontierUexplored.add(neighbour.value)
                     #self.parentOf[neighbour] = currState
             
         return False
-
-
-    def pos(self, index):   return index // 3, index % 3
-
-
-    def manhattanDistance(self, currentPos, desiredPos):
-        desired_posy, desired_posx = self.pos(desiredPos)
-        current_posy, current_posx = self.pos(currentPos)
-        return abs(desired_posx - current_posx) + abs(desired_posy - current_posy)
-        
-
-    def ManhattanDistance(self, state: State, i):
-        tile = int(state.value[i])
-        #state.Manhattans = [0, 1, 1, 0, 0, 1, 0, 0, 0]
-        state.heuristics[tile] = abs(tile // 3 + tile % 3 - i // 3 - i % 3)
-        state.heuristic = sum(state.heuristics)
-        
-
-    def initialManhattan(self, initialstate: State):
-        heuristics = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        for i in range(9):
-            if(initialstate.value[i] == '0'): continue
-            heuristics[int(initialstate.value[i])] = self.manhattanDistance(i, int(initialstate.value[i]))
-        initialstate.heuristic = sum(heuristics)
