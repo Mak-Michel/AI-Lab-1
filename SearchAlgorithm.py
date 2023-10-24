2ad10da1f8178a8cd34d1ef87a1552d1da8c3137
Maro1123 <96551919+Maro1123@users.noreply.github.com>
2023-10-24 15:49:50 +0300

    test #2

--- SearchAlgorithm.py
+++ SearchAlgorithm.py
@@ -9,10 +9,6 @@ class SearchAlgorithm:
         self.nodesExpanded = set()
         self.maxDepth = 0
         self.goal: State = None
-        #self.parentOf = dict()
-
-    def goalTest(self, state: State):
-        return state.value == self.goalTest
     
     def swapAndAppend(self, state: State, index1, index2, neighbours):
         temp = list(state.value)
@@ -32,25 +28,3 @@ class SearchAlgorithm:
         if indexOf0 < 6: self.swapAndAppend(currState, indexOf0, indexOf0 + 3, neighbours)
         if indexOf0 > 2: self.swapAndAppend(currState, indexOf0, indexOf0 - 3, neighbours)
         return neighbours
-
-    def printState(self, tuple):
-        strList = list(tuple[0])
-        strList[tuple[1]] = " "
-        print(" ―――――――")
-        for i in range(0, 9, 3):
-            print(f"│ {strList[i]} │ {strList[i+1]} │ {strList[i+2]} │")
-            print(" ―――――――")
-        print()
-
-    def printPath(self, state: State):
-        stackPath = deque()
-        stackPath.append((state.value, state.indexOf0))
-        #parent = self.parentOf[state]
-        currState = state.parent
-        while currState is not None:
-            stackPath.append((currState.value, currState.indexOf0))
-            currState = currState.parent
-            #parent = self.parentOf[parent]
-
-        while stackPath:
-            self.printState(stackPath.pop())
--- State.py
+++ State.py
@@ -2,9 +2,9 @@ class State:
 
     def __init__(self, value="", indexOf0=0):
         self.value = value
+        self.indexOf0 = indexOf0
         self.parent: State = None
         self.cost = 0
-        self.depth = 0
         self.heuristic = 0
 
     def __lt__(self, other):
